60df31892f4b8fdea261cdb4d3e0430e3cb1d88d
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-08-14 19:13:09 -0300

    update create item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app-lista-de-compras';
 
-  constructor() { }
+  listaDeCompra!: Array<Item> // Item foi definido pela interface.
+
+  constructor(
+    private listaService: ListaDeCompraService  // Declarado um atributo lista Service do tipo ListaDeCompraService para podermos hookar o método necessário getListaDeCompra.
+  )
+
+  { }
+
+  ngOnInit(): void {
+    this.listaDeCompra = this.listaService.getListaDeCompra();
+    return console.log(this.listaDeCompra);
+  }
+
 }
+
+
--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent implements OnInit, OnChanges{
+
+  @Input() item!: Item  // Com essa propriedado podemos atribuir valores a esta instacia através, por exemplo, de property binding. Item irá receber os valores de atributos da base de dados.
 
   faPen = faPen;
   faTrash = faTrash
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnChanges(): void {
+    console.log("OnChanges")
+  }
+
+  ngOnInit(): void {
+    console.log("OnInit")
+   }
 
 }
