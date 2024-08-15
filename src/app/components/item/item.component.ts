1d45f44dd99dd6c52eb6d2be2c8d1bf8f291443f
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-08-15 19:22:38 -0300

    Update OnDestroy

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, OnDestroy } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Item } from 'src/app/interfaces/iItem';
 
@@ -7,10 +7,11 @@ import { Item } from 'src/app/interfaces/iItem';
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit, OnChanges{
+export class ItemComponent implements OnInit, OnChanges, OnDestroy{
 
-  @Input() item!: Item  // Com essa propriedado podemos atribuir valores a esta instacia através, por exemplo, de property binding. Item irá receber os valores de atributos da base de dados.
+  @Input() item!: Item;  // Com essa propriedado podemos atribuir valores a esta instacia através, por exemplo, de property binding. Item irá receber os valores de atributos da base de dados.
   @Output() emitindoItemParaEditar = new EventEmitter();
+  @Output() emitindoIdParaDeletar = new EventEmitter();
 
   faPen = faPen;
   faTrash = faTrash;
@@ -28,4 +29,22 @@ export class ItemComponent implements OnInit, OnChanges{
    editarItem(){
     this.emitindoItemParaEditar.emit(this.item);
    }
+
+   checarItem() {
+    if(this.item.comprado == true){
+      this.item.comprado = false;
+    }else{
+      this.item.comprado = true;
+    }
+  }
+
+  deletarItem(){
+    console.log('Estão tentando me calar.')
+    this.emitindoIdParaDeletar.emit(this.item.id);
+  }
+
+  ngOnDestroy(){
+    console.log('Conseguiram me calar.')
+  }
+
 }
